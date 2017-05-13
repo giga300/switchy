77addfe233a6047c1561ca7e2ebc70e666af1986
Giga300 <gigaagent@gmail.com>
2017-05-13 18:41:40 +0200

    Base + Say Command

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+const config = require("./config.json")
+
+bot.on('ready', () => {
+    console.log("Switchy is operational")
+})
+
+bot.on("guildMemberAdd", member => {
+    let guild = member.guild
+    guild.defaultChannel.sendMessage(`Bienvenue ${member.user} sur le serveur.`)
+})
+
+bot.on("message", message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(config.prefix)) return
+    
+    let command = message.content.split(" ")[0];
+    command = command.slice(config.prefix.length)
+
+    let args = message.content.split(" ").slice(1)
+
+    /**
+     * SAY COMMAND / ADMIN COMMAND
+     */
+    if (command === "say") {
+        let modRole = message.guild.roles.find("name", "bot-op")
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("[:x:] Vous n'avez pas la permission d'utiliser cette commande !").catch(console.error)
+        }
+        message.channel.sendMessage(args.join(" "))
+    }
+
+})
+
+
+
+bot.login(config.token)
